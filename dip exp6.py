c014d9e631efb913610255699027a3e834e7284f
vigneshwar-24 <77089276+vigneshwar-24@users.noreply.github.com>
2022-05-10 22:32:59 +0530

    Add files via upload

---
+++ dip exp6.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+image1=cv2.imread("im1.jpg")
+image2=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)
+
+
+# In[2]:
+
+
+kernel=np.ones((11,11),np.float32)/121
+image3=cv2.filter2D(image2,-1,kernel)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(image2)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(image3)
+plt.title("Average Filter Image")
+plt.axis("off")
+plt.show()
+
+
+# In[3]:
+
+
+kernel1=np.array([[1,2,1],[2,4,2],[1,2,1]])/16
+image3=cv2.filter2D(image2,-1,kernel1)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(image2)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(image3)
+plt.title("Weighted Average Filter Image")
+plt.axis("off")
+plt.show()
+
+
+# In[4]:
+
+
+gaussian_blur=cv2.GaussianBlur(image2,(33,33),0,0)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(image2)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(gaussian_blur)
+plt.title("Gaussian Blur")
+plt.axis("off")
+plt.show()
+
+
+# In[5]:
+
+
+median=cv2.medianBlur(image2,13)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(image2)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(gaussian_blur)
+plt.title("Median Blur")
+plt.axis("off")
+plt.show()
+
+
+# In[6]:
+
+
+kernel2=np.array([[-1,-1,-1],[2,-2,1],[2,1,-1]])
+image3=cv2.filter2D(image2,-1,kernel2)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(image2)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(image3)
+plt.title("Laplacian Kernel")
+plt.axis("off")
+plt.show()
+
+
+# In[7]:
+
+
+laplacian=cv2.Laplacian(image2,cv2.CV_64F)
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.imshow(image2)
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(laplacian)
+plt.title("Laplacian Operator")
+plt.axis("off")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
